554ba0069160d2f6e078f2f2dc95aa6eb9016036
hai99 <thaiviethai99@gmail.com>
2020-03-06 10:59:26 +0700

    first code

---
+++ src/components/index.component.js
@@ -0,0 +1,83 @@
+// index.component.js
+import React, {
+    Component
+} from 'react';
+import API from '../axios';
+import TableRow from './TableRow';
+import Pagination from "react-js-pagination";
+export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [],
+            activePage: 0,
+            itemsCountPerPage: 0,
+            totalItemsCount: 0,
+            pageRangeDisplayed: 5,
+        };
+        //this.deleteSearch=this.deleteSearch.bind(this)
+        this.handlePageChange = this.handlePageChange.bind(this);
+    }
+    handlePageChange(pageNumber) {
+        var randDom = Math.random();
+        API.get(`list?v=${randDom}&page=${pageNumber}`).then(response => {
+            this.setState({
+                news: response.data.data,
+                activePage: response.data.current_page,
+            });
+        })
+    }
+    componentDidMount() {
+        API.get('/list').then(response => {
+            this.setState({
+                news: response.data.data,
+                activePage: response.data.current_page,
+	            itemsCountPerPage: response.data.per_page,
+	            totalItemsCount: response.data.total,
+            });
+        }).catch(function(error) {
+            console.log(error);
+        })
+    }
+    tabRow() {
+        return this.state.news.map(function(object, i) {
+            return <TableRow obj = {
+                object
+            }
+            key = {
+                i
+            }
+            />;
+        });
+    }
+    render() {
+        return ( < div > < h3 align = "center" > News List < /h3> < table className = "table table-striped"
+            style = {
+                {
+                    marginTop: 20
+                }
+            } > < thead > < tr > < th > Id < /th> < th > Title < /th> < /tr> < /thead> < tbody > {
+                this.tabRow()
+            } < /tbody> < /table> < div style = {
+                {
+                    float: 'right'
+                }
+            } > < Pagination activePage = {
+                this.state.activePage
+            }
+            itemsCountPerPage = {
+                this.state.itemsCountPerPage
+            }
+            totalItemsCount = {
+                this.state.totalItemsCount
+            }
+            pageRangeDisplayed = {
+                5
+            }
+            onChange = {
+                this.handlePageChange
+            }
+            itemClass = "page-item"
+            linkClass = "page-link" / > < /div> < /div>);
+    }
+}
